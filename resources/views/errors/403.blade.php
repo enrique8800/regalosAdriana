6fa56858195d5ee25dc7864c953121817f70dca3
Enrique <enrique8800@gmail.com>
2021-03-02 20:48:42 +0100

    vista errores

--- resources/views/errors/403.blade.php
+++ resources/views/errors/403.blade.php
@@ -1,3 +1,5 @@
+@extends('errors::layout')
+@section('title', '403')
 <!DOCTYPE html>
 <html lang="en">
 
--- resources/views/errors/500.blade.php
+++ resources/views/errors/500.blade.php
@@ -1,3 +1,5 @@
+@extends('errors::layout')
+@section('title', '500')
 <!DOCTYPE html>
 <html lang="en">
 
